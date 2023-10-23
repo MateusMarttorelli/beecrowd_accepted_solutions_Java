fa6088fd41f1d869e6c2a5335ecccd8b6f7c3ae7
Mateus Marttorelli <mateus.hmarttorelli@gmail.com>
2023-10-23 15:16:58 -0300

    Updating

---
+++ bee_1095.java
@@ -0,0 +1,14 @@
+public class bee_1095
+{
+    public static void main(String[] args)
+    {
+        int i = 1, j = 60;
+
+        while (j >= 0)
+        {
+            System.out.printf("I=%d J=%d\n", i, j);
+            i += 3;
+            j -= 5;
+        }
+    }
+}
